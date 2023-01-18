bd0411f79466ee03b22275c6a70ca70c8826c087
andrejskvorcov <andreys@trueflip.io>
2023-01-18 12:41:40 +0300

    GAME-1: edit global app

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,14 @@
-import { Application } from "@pixi/app";
-import { Sprite } from "@pixi/sprite";
-import { Texture } from "@pixi/core";
+import {Application} from "@pixi/app";
+import {Sprite} from "@pixi/sprite";
+import {Texture} from "@pixi/core";
+import {Container} from "@pixi/display";
 
 import Button from "./components/Button";
+
 export default class App extends Application {
   constructor() {
     super({
-      width: window.innerWidth,
-      height: window.innerHeight,
+      width: window.innerWidth, height: window.innerHeight,
     });
 
     document.body.appendChild(this.view); // Put Canvas tag in the body
@@ -18,64 +19,64 @@ export default class App extends Application {
   }
 
   init() {
-    this.loader.add("button", "./assets/button.png");
     this.loader.add("buttonBuy", "./assets/buttonBuy.png");
     this.loader.add("buttonInvite", "./assets/buttonInvite.png");
+    this.loader.add("background", "./assets/background.png");
     this.loader.load(this.setup.bind(this));
   }
 
   setup() {
-    this.button1 = new Button({
-      nameTexture: "button",
-      label: "Купить монеты",
-      width: 300,
-      height: 80,
-      x: 200,
-      y: 200,
-      onTap: () => console.log("Play"),
-    });
+    // create background
+    this.background = new Sprite.from("background");
+    this.background.width = window.innerWidth;
+    this.background.height = window.innerHeight;
 
-    console.log('---button1---', this.button1)
+    this.stage.addChild(this.background);
+
+    this.createHeader()
+
+    this.onResize();
+  }
+
+  createHeader (){
+    // create header container
+    this.headerContainer = new Container();
 
     this.buttonBuy = new Button({
-      nameTexture: "buttonBuy",
-      label: "Купить  монеты",
-      width: 300,
       height: 80,
+      label: "Купить монеты",
+      nameTexture: "buttonBuy",
+      width: 300,
       x: 400,
       y: 400,
-      onTap: () => console.log("Play"),
+      onTap: () => console.log("Купить монеты"),
     });
 
     this.buttonInvite = new Button({
-      nameTexture: "buttonInvite",
-      label: "Пригласить",
-      width: 300,
       height: 80,
+      label: "Пригласить",
+      nameTexture: "buttonInvite",
+      width: 300,
       x: 600,
       y: 600,
-      onTap: () => console.log("Play"),
+      onTap: () => console.log("Пригласить"),
     });
 
-    this.stage.addChild(this.button1, this.buttonBuy, this.buttonInvite);
-    this.onResize();
+    this.headerContainer.addChild(this.buttonBuy, this.buttonInvite)
+
+    this.stage.addChild(this.headerContainer);
   }
 
   onResize() {
     this.renderer.resize(window.innerWidth, window.innerHeight);
 
-    const width = this.renderer.width;
-
-    const height = this.renderer.height;
-
-    const btnMargin = 5;
-
+    // const width = this.renderer.width;
+    // const height = this.renderer.height;
+    // const btnMargin = 5;
     // this.button1.x = width * 0.5;
     // this.button1.y = height * 0.5 - this.button1.height * 0.5 - btnMargin;
-
     // this.buttonBuy.x = width * 0.5;
     // this.buttonBuy.y = height * 0.5 + this.buttonBuy.height * 0.5 + btnMargin;
-
     // this.buttonInvite.x = width * 0.5;
     // this.buttonInvite.y = height * 0.5 + this.buttonBuy.height * 2 + btnMargin;
   }
